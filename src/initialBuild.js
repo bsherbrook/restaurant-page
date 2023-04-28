3f7eec39e29d48240d6542b868527cbeea6188a9
bsherbrook <bsherbrook@gmail.com>
2023-04-28 18:35:18 -0400

    add light styling

--- src/initialBuild.js
+++ src/initialBuild.js
@@ -1,30 +1,38 @@
 const pageBody = document.getElementById("content");
 const pageHead = document.createElement("header");
-const titleCard = document.createElement("p");
+const titleCard = document.createElement("h2");
+const buttonBox= document.createElement("div");
 const tabOne = document.createElement("button");
 const tabTwo = document.createElement("button");
 const tabThree = document.createElement("button");
 //header
-titleCard.innerHTML = "Restaurant Name";
-tabOne.innerHTML = "Tab 1";
-tabTwo.innerHTML = "Tab 2";
-tabThree.innerHTML = "Tab 3";
+titleCard.innerHTML = "Gabriel's Southern Kitchen";
+tabOne.innerHTML = "Home";
+tabTwo.innerHTML = "Menu";
+tabThree.innerHTML = "Contact Us";
 tabOne.setAttribute('id','tabOne');
 tabTwo.setAttribute('id','tabTwo')
 tabThree.setAttribute('id','tabThree');
+buttonBox.setAttribute('id', 'buttonBox')
 pageHead.appendChild(titleCard);
-pageHead.appendChild(tabOne);
-pageHead.appendChild(tabTwo);
-pageHead.appendChild(tabThree);
+buttonBox.appendChild(tabOne);
+buttonBox.appendChild(tabTwo);
+buttonBox.appendChild(tabThree);
+pageHead.appendChild(buttonBox);
 pageBody.appendChild(pageHead);
 //body
 const contentBody = document.createElement("div");
 contentBody.setAttribute("id", "content-body");
+const backgroundPic= document.createElement("img");
+backgroundPic.src= "./images/aditya-joshi-wood.jpg";
+backgroundPic.setAttribute('id', 'backgroundPic');
+pageBody.appendChild(backgroundPic);
 const quoteOne = document.createElement("p");
 const quoteTwo = document.createElement("p");
 const contentPic = document.createElement("img");
-//contentPic.src=
-//contentPic.alt=
+contentPic.src= "./images/febrian-zakaria-chef.jpg"
+contentPic.alt="Chef Gabriel"
+contentPic.setAttribute('id','chefPic');
 const orderNow = document.createElement("p");
 quoteOne.innerHTML = "Best food this side of Mississip!";
 quoteTwo.innerHTML = `"You won't regret it" -John Foodguy`;
